62f3b3b8d3fa1a6572e44ccc4f2411e5d1c4f5a1
Mehdi <smuhammadmehdi8@gmail.com>
2026-04-27 15:20:20 -0500

    starting point

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,7 @@ function App() {
   return (
 
     <body> 
-      <h1>LeChatelier Simulation</h1>
-      <div class="slidecontainer">
-        <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
-      </div>
+      <h1>LeChatelier Simulation</h1>\
     </body>
 
       
