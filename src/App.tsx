c73cd330784fd7057da0c53a2e3bfbb38d33ff5d
Mehdi <smuhammadmehdi8@gmail.com>
2026-04-29 15:22:12 -0500

    started pixijs canvas injection

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,63 @@
+import {useEffect, useRef} from 'react';
+import * as pixi from 'pixi.js';
+import {Application} from 'pixi.js';
 import "./index.css"
 
+
+
+
 function App() {
+
+  const pixiContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    (async() => {
+  
+    const app = new Application();
+    await app.init(
+      {
+        //resizeTo: window,
+        width: 1000,
+        height: 500,
+        backgroundColor: 0x1e2a38,
+        backgroundAlpha: 0.5,
+        antialias: true
+      }
+
+    ); 
+
+    pixiContainerRef.current?.appendChild(app.canvas);
+
+    /*
+    if (pixiContainerRef.current) {
+      pixiContainerRef.current.appendChild(app.canvas);
+    } */
+
+})();
+
+
+
+
+    console.log("div ready for pixijs");
+  }, []); 
+
+
   return (
 
-    <body> 
-      <h1>LeChatelier Simulation</h1>\
-    </body>
+
+
+    <div> 
+      <h1>LeChatelier Simulation</h1>
+
+      <div ref={pixiContainerRef}></div>
+      
+    </div>
+
+
 
       
   )
+
 
 }
 
