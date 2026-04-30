cf1121f5d1829af64b8dd3c0613d09e8fcdbf193
Muhammad Mehdi <smuhammadmehdi8@gmail.com>
2026-04-30 17:49:04 -0500

    engines

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,6 @@ function App() {
 
     <body> 
       <h1>LeChatelier Simulation</h1>
-      <div class="slidecontainer">
-        <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
-      </div>
     </body>
 
       
